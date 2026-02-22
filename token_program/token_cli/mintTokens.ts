2fb80b78a9f5de0ed5a9c2add173f0cfbe349771
Vishal <vishal.v@tripfactory.travel>
2026-02-23 00:16:40 +0530

    minting tokens on chain

---
+++ token_program/token_cli/mintTokens.ts
@@ -0,0 +1,33 @@
+/**
+ * @param PRIVATE_KEY = 5AVrh2JnU1rvSRjGLzdTxp7EnVALizrDYfcqQzAcx1Bo6sf1RjSN6x1TFRiZ4doy2wnMuTbEtgAwMqgYD6ybfvdy
+ * @param MINT_ADDRESS = J3744UZ5HBFr8oB3aK7gSQJqZ3phoZgJ6M92uCKptcLU
+ */
+
+/**
+ * 
+vishal@Vishals-MacBook-Pro ~ % spl-token mint J3744UZ5HBFr8oB3aK7gSQJqZ3phoZgJ6M92uCKptcLU 100
+Minting 100 tokens
+  Token: J3744UZ5HBFr8oB3aK7gSQJqZ3phoZgJ6M92uCKptcLU
+  Recipient: 9z3Br1MmKwhvyaeG49aqpSXCAppUngtVXpmLy7DMVs9T
+
+Signature: 4Qhr7JEyERctgDZW2Nc8JrtCFWQZCrkqV44RmExE7WtqVKWRLKLrLL9iV96QzsyFr4NCgk6ZTDLsTDF5xLknNN4s
+
+vishal@Vishals-MacBook-Pro ~ % spl-token accounts
+Token                                         Balance
+-----------------------------------------------------
+J3744UZ5HBFr8oB3aK7gSQJqZ3phoZgJ6M92uCKptcLU  100
+
+vishal@Vishals-MacBook-Pro ~ % 
+
+
+ */
+
+/***
+ * whats happening here is i am minting the token `J3744UZ5HBFr8oB3aK7gSQJqZ3phoZgJ6M92uCKptcLU`
+ * here this key is that token whose owner is my keypair in `my-wallet.json`
+ 
+
+
+ * 9z3Br1MmKwhvyaeG49aqpSXCAppUngtVXpmLy7DMVs9T -> this is the ATA account
+   only for this mint `J3744UZ5HBFr8oB3aK7gSQJqZ3phoZgJ6M92uCKptcLU` whose owner is my keypair 
+ */
